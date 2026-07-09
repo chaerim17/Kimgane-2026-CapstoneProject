7ff83207978d459b0a852d74026cd7ee1caa47d0
goqiewd <cheari1018@gmail.com>
2026-07-10 02:53:57 +0900

    feat(server): 로그인 및 아바타 정보 동기화 구현
    - IO_RECV 패킷 수신 처리 추가
    - C2S_LOGIN 패킷 처리 구현
    - SendLoginSuccess 추가
    - SendAvatarInfo 추가
    - IOCP 기반 비동기 송신 처리 추가
    - Session 기반 패킷 송수신 구조 정리

--- Sever/KimganeServer/main.cpp
+++ Sever/KimganeServer/main.cpp
@@ -57,7 +57,13 @@ public:
     SOCKET mClient;
     int mId;
     bool mIsConnected;
-    
+
+    ExpOver mRecvOver;
+    int mPrevRecv{};
+    char mUserName[MAX_NAME_LEN];
+
+    float mX, mY, mZ;
+
     Session()
     {
         mClient = INVALID_SOCKET;
@@ -69,9 +75,107 @@ public:
         if (mIsConnected)
             closesocket(mClient);
     }
+
+    void DoRecv()
+    {
+        std::cout << "DoRecv called for client[" << mId << "]\n";
+        DWORD recv_flag = 0;
+        ZeroMemory(&mRecvOver.mOver, sizeof(mRecvOver.mOver));
+        mRecvOver.mIoType = IO_RECV;
+        // 패킷 재조립
+        mRecvOver.mWsa.len = BUF_SIZE - mPrevRecv;
+        mRecvOver.mWsa.buf = mRecvOver.mBuffer + mPrevRecv;
+        WSARecv(mClient, &mRecvOver.mWsa, 1, 0, &recv_flag, &mRecvOver.mOver, nullptr);
+    }
+
+    void DoSend(int size, char* buffer)
+    {
+        ExpOver* o = new ExpOver(IO_SEND);
+        o->mWsa.len = size;
+        memcpy(o->mBuffer, buffer, size);
+        WSASend(mClient, &o->mWsa, 1, 0, 0, &o->mOver, nullptr);
+    }
+
+    void SendAvatarInfo()
+    {
+        S2C_AvatarInfo avatarPacket;
+        avatarPacket.size = sizeof(S2C_AvatarInfo);
+        avatarPacket.type = S2C_AVATAR_INFO;
+        avatarPacket.playerId = mId;
+        avatarPacket.x = mX;
+        avatarPacket.y = mY;
+        avatarPacket.z = mZ;
+        DoSend(avatarPacket.size, reinterpret_cast<char*>(&avatarPacket));
+    }
+
+    void SendMovePlayer(int moverId);
+    void SendAddPlayer(int playerId);
+
+    void SendLoginSuccess()
+    {
+        S2C_LoginResult loginResultPacket;
+        loginResultPacket.size = sizeof(S2C_LoginResult);
+        loginResultPacket.type = S2C_LOGIN_RESULT;
+        loginResultPacket.success = true;
+        strncpy_s(loginResultPacket.message, "Login successful.", sizeof(loginResultPacket.message));
+        DoSend(loginResultPacket.size, reinterpret_cast<char*>(&loginResultPacket));
+    }
+    void SendRemovePlayer(int playerId)
+    {
+        S2C_RemovePlayer removePlayerPacket;
+        removePlayerPacket.size = sizeof(S2C_RemovePlayer);
+        removePlayerPacket.type = S2C_REMOVE_PLAYER;
+        removePlayerPacket.playerId = playerId;
+        DoSend(removePlayerPacket.size, reinterpret_cast<char*>(&removePlayerPacket));
+    }
+
+    void ProcessPacket(unsigned char* packet);
 };
 
 std::array<std::unique_ptr<Session>, MAX_PLAYERS> clients;
+
+void Session::ProcessPacket(unsigned char* packet)
+{
+    PACKET_TYPE type = *reinterpret_cast<PACKET_TYPE*>(&packet[1]);
+    switch (type)
+    {
+    case C2S_LOGIN:
+    {
+        C2S_Login* loginPacket = reinterpret_cast<C2S_Login*>(packet);
+        strncpy_s(mUserName, loginPacket->username, MAX_NAME_LEN);
+        std::cout << "Client[" << mId << "] Login: " << mUserName << std::endl;
+
+        SendAvatarInfo();
+        SendLoginSuccess();
+        break;
+    }
+
+    case C2S_MOVE:
+    {
+        C2S_Move* movePacket = reinterpret_cast<C2S_Move*>(packet);
+        DIRECTION dir = movePacket->direction;
+
+        // TODO: Update player position 계산
+        switch (dir)
+        {
+        case UP:
+            break;
+        case DOWN:
+            break;
+        case LEFT:
+            break;
+        case RIGHT:
+            break;
+        }
+
+        std::cout << "Player[" << mId << "] moved \n";
+        break;
+    }
+    default:
+        std::cout << "Unknown packet type received from player[" << mId << "].\n";
+        break;
+    }
+}
 
 int main()
 {
@@ -91,8 +195,8 @@ int main()
     SOCKET clientSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 
     ExpOver acceptOver(IO_ACCEPT);
-    AcceptEx(serverSocket, clientSocket, acceptOver.mBuffer, 0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16, NULL,
-             &acceptOver.mOver);
+    AcceptEx(serverSocket, clientSocket, acceptOver.mBuffer, 0, sizeof(SOCKADDR_IN) + 16, sizeof(SOCKADDR_IN) + 16,
+             NULL, &acceptOver.mOver);
 
     for (int playerID = 0;;)
     {
@@ -111,8 +215,8 @@ int main()
             clients[clientId]->mIsConnected = false;
             for (auto& cl : clients)
                 if (true == cl->mIsConnected)
-                    //cl->send_remove_player(clientId);
-            closesocket(clients[clientId]->mClient);
+                    // cl->send_remove_player(clientId);
+                    closesocket(clients[clientId]->mClient);
             clients[clientId]->mClient = INVALID_SOCKET;
             continue;
         }
@@ -131,13 +235,58 @@ int main()
             clients[playerID] = std::move(session);
 
             CreateIoCompletionPort((HANDLE)clientSocket, hIocp, playerID, 0);
+
+            std::cout << "Register Recv\n";
+            clients[playerID]->DoRecv();
+            std::cout << "Recv Registered\n";
             ++playerID;
             break;
         }
         case IO_RECV:
-        break;
+        {
+            int playerId = static_cast<int>(clientId);
+
+            if (numBytes == 0)
+            {
+                std::cout << "Client[" << playerId << "] Disconnected\n";
+                clients[playerId]->mIsConnected = false;
+                closesocket(clients[playerId]->mClient);
+                clients[playerId].reset();
+                break;
+            }
+            std::cout << "Client[" << playerId << "] Recv: " << numBytes << std::endl;
+
+            Session* session = clients[playerId].get();
+            unsigned char* packetPtr = reinterpret_cast<unsigned char*>(expOver->mBuffer);
+            int dataSize = numBytes + session->mPrevRecv;
+
+            while (dataSize > 0)
+            {
+                const int packetSize = packetPtr[0];
+                if (packetSize > dataSize)
+                    break;
+
+                session->ProcessPacket(packetPtr);
+                packetPtr += packetSize;
+                dataSize -= packetSize;
+            }
+
+            if (dataSize > 0)
+            {
+                memmove(session->mRecvOver.mBuffer, packetPtr, dataSize);
+                session->mPrevRecv = dataSize;
+            }
+            else
+            {
+                session->mPrevRecv = 0;
+            }
+            session->DoRecv();
+
+            break;
+        }
         case IO_SEND:
-        break;
+            delete expOver;
+            break;
         default:
             std::cout << "Unknown IO type." << std::endl;
             exit(-1);
