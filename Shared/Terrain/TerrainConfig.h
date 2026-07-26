44ff9aac77ad92a201e4f3dee4eb035996de430a
goqiewd <cheari1018@gmail.com>
2026-07-26 21:56:51 +0900

    refactor(server): Terrain의 엔진 의존성 제거
    - DirectX 의존성 제거
    - AssetPathResolver 제거
    - 서버 전용 Terrain 구조로 분리
    - HeightMap 로직 이식 기반 구성

--- Shared/Terrain/TerrainConfig.h
+++ Shared/Terrain/TerrainConfig.h
@@ -1,13 +1,17 @@
 #pragma once
 #include <cstdint>
 
-namespace Shared
+namespace TerrainConfig
 {
+    // Heightmap 파일
+    inline constexpr wchar_t TERRAIN_RAW_PATH[] = L"terrain_100x100.raw";
+
+    // Heightmap 크기
     inline constexpr uint32_t TERRAIN_WIDTH = 513;
     inline constexpr uint32_t TERRAIN_LENGTH = 513;
 
+    // 월드 변환 정보
     inline constexpr float CELL_SPACING = 0.1953125f;
     inline constexpr float HEIGHT_SCALE = 20.0f;
 
-    inline constexpr wchar_t TERRAIN_RAW_PATH[] = L"terrain_100x100.raw";
-} // namespace Shared
+}
