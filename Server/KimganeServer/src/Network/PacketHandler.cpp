e2ae3ae1453f24a7eaa1034a3d3681f09342895a
goqiewd <cheari1018@gmail.com>
2026-09-11 01:46:47 +0900

    feat(server): 발사 요청 패킷 수신 처리 추가
    - 현재 shoot패킷을 수신 이후 명중 처리 로직을 구현하지 않음.
    - 현재 shoot 패킷을 제대로 수신했는지에 대한 확인만 완료한 상태

--- Server/KimganeServer/src/Network/PacketHandler.cpp
+++ Server/KimganeServer/src/Network/PacketHandler.cpp
@@ -1,5 +1,7 @@
 #include "PacketHandler.h"
 
+#include <cmath>
+
 #include "../Core/Session.h"
 #include "../Core/Server.h"
 #include "../Npc/NpcSetting.h"
@@ -33,6 +35,10 @@ void PacketHandler::HandlePacket(Session* session, unsigned char* packet)
 
     case C2S_PLAYER_STATE:
         HandlePlayerState(session, packet);
+        break;
+
+    case C2S_SHOOT:
+        HandleShoot(session, packet);
         break;
     }
 }
@@ -175,4 +181,14 @@ void PacketHandler::HandlePlayerState(Session* session, unsigned char* packet)
                   << "Client(" << p->x << ", " << p->y << ", " << p->z << ") "
                   << "Error=" << error << '\n';*/
     }
+}
+
+void PacketHandler::HandleShoot(Session* session, unsigned char* packet)
+{
+    auto* shootPacket = reinterpret_cast<C2S_Shoot*>(packet);
+    const Vec3& direction = shootPacket->direction;
+
+    std::cout << "[SHOOT] Received: sessionId=" << session->GetId()
+              << " playerId=" << shootPacket->playerId
+              << " direction=(" << direction.x << ", " << direction.y << ", " << direction.z << ")\n";
 }
